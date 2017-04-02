29dd8e85ebbecb66a644e91b418baf4c1d2f2e2c
Keith Hitchcock <krjhitch@gmail.com>
2017-04-02 10:24:28 -0400

    tests

--- tests/AppVeyor.ps1
+++ tests/AppVeyor.ps1
@@ -1,3 +1,4 @@
 Copy-Item -Path '.\' -Destination 'C:\Program Files\WindowsPowerShell\Modules' -Recurse
 Import-Module xWindows -Verbose
 Get-DSCResource -Module xWindows
+Get-Module -ListAvailable -FullyQualifiedName xWindows -Verbose
