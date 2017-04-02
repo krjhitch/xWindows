dac92a2e7cc109542d2bf6f1c576710d8937b558
Keith Hitchcock <krjhitch@gmail.com>
2017-04-02 10:17:01 -0400

    xwindowsaudit tests

---
+++ tests/xWindowsAudit.tests.ps1
@@ -0,0 +1,35 @@
+configuration 'xWindowsAuditTests' {
+    Import-DscResource -ModuleName xWindows
+    node 'localhost' {
+        xWindowsAudit 'Test1' { #Test setting both to true
+            AuditSubCategory = 'IPsec Main Mode'
+            Success          = $true
+            Failure          = $true
+        }
+
+        xWindowsAudit 'Test2' { #Test setting both to false
+            AuditSubCategory = 'Network Policy Server'
+            Success = $false
+            Failure = $false
+        }
+
+        xWindowsAudit 'Test3' { #Test setting one to true and one to false
+            AuditSubCategory = 'File System'
+            Success = $false
+            Failure = $true
+        }
+
+        xWindowsAudit 'Test4' { #Test setting one value and not setting the other
+            AuditSubCategory = 'Certification Services'
+            Success = $true
+        }
+
+        xWindowsAudit 'Test5' { #Test setting one value and not setting the other
+            AuditSubCategory = 'Sensitive Privilege Use'
+            Failure = $true
+        }
+    }
+}
+
+xWindowsAuditTests
+Start-DscConfiguration .\xWindowsAuditTests -Wait -Force -verbose -ErrorAction Stop
